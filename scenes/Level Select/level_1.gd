5b3cb7b0d4c5b94a9023dddb3c0960fab151c494
Dominic Webster <dominicbweiss@gmail.com>
2026-03-24 12:00:33 -0400

    complete game flow

--- scenes/Level Select/level_1.gd
+++ scenes/Level Select/level_1.gd
@@ -6,15 +6,16 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	forward_button.pressed.connect(_on_forward_button_pressed)
+	level_select_button.pressed.connect(_level_select_button_pressed)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _on_forward_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Level Select/level_2.tscn")
 
-#func _level_select_button_pressed():
-#	get_tree().change_scene_to_file("")
+func _level_select_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/arena.tscn")
