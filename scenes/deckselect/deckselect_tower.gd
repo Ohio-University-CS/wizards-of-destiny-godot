6e6c9e768048952c3f37995be33b0df9b061108a
John Hepke <johnhepke@Johns-MacBook-Pro.local>
2026-03-26 11:46:37 -0400

    Tower done

---
+++ scenes/deckselect/deckselect_tower.gd
@@ -0,0 +1,23 @@
+# Emperor
+extends Control
+
+# Button nodes
+@onready var back_button : Button = $Buttons/Back
+@onready var forward_button : Button = $Buttons/Forward
+
+func _ready():
+	# Connect button presses
+	if not back_button.pressed.is_connected(_on_back_pressed):
+		back_button.pressed.connect(_on_back_pressed)
+	if not forward_button.pressed.is_connected(_on_forward_pressed):
+		forward_button.pressed.connect(_on_forward_pressed)
+
+# Go back to the first deck select screen
+func _on_back_pressed():
+	print("Back pressed")
+	get_tree().change_scene_to_file("res://scenes/deckselect/deckselect_star.tscn")
+
+# Go forward to the next deck select screen
+func _on_forward_pressed():
+	print("Forward pressed")
+	get_tree().change_scene_to_file("res://scenes/deckselect/deckselect_death.tscn")
