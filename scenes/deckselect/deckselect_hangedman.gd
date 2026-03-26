61cbfacbc3f32e3e81f91b121748d8e0487a1776
John Hepke <johnhepke@Johns-MacBook-Pro.local>
2026-03-26 12:07:15 -0400

    Arena button

--- scenes/deckselect/deckselect_hangedman.gd
+++ scenes/deckselect/deckselect_hangedman.gd
@@ -3,16 +3,22 @@ extends Control
 
 # Button nodes
 @onready var back_button : Button = $Buttons/Back
+@onready var level_select_button : Button = $Buttons/"Level Select"
 
 func _ready():
 	# Connect button presses
 	if not back_button.pressed.is_connected(_on_back_pressed):
 		back_button.pressed.connect(_on_back_pressed)
+		
+	if not level_select_button.pressed.is_connected(_on_level_select_pressed):
+		level_select_button.pressed.connect(_on_level_select_pressed)
 
-
-# Go back to the first deck select screen
+# Go back to previous deck
 func _on_back_pressed():
 	print("Back pressed")
 	get_tree().change_scene_to_file("res://scenes/deckselect/deckselect_death.tscn")
 
-# Go forward to the next deck select screen
+# Go to arena (level select)
+func _on_level_select_pressed():
+	print("Level Select pressed")
+	get_tree().change_scene_to_file("res://scenes/arena.tscn")
