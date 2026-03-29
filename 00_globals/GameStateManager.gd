121278103028dc9d315af1d4d00e6042725d6655
kaibehrens <kb252523@ohio.edu>
2026-03-29 16:01:17 -0400

    Added GameStateManager to globals

---
+++ 00_globals/GameStateManager.gd
@@ -0,0 +1,16 @@
+
+
+
+func _ready() -> void:
+	var player_dict = get_run_player_state()
+	for key in player_dict:
+		print(key + " : " + player_dict[key])
+	
+
+
+func get_run_player_state() -> Dictionary:
+	var player_script = load("res://scenes/player/Player.gd")
+	var player_state = {}
+	player_state.
+	
+	return {}
