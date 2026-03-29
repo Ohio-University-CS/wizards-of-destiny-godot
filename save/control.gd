214921fff2a56f172e76702d606ab1d9863b53ff
kaibehrens <kb252523@ohio.edu>
2026-03-29 14:47:04 -0400

    Updated exports and control

--- save/control.gd
+++ save/control.gd
@@ -1,16 +1,30 @@
 extends Control
-
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
 
-	pass
+var player_data
+
+
+func _ready() -> void:
+	load_player_data("save_1")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
 
-#func load_settings() -> bool:
-	#var settings_file = file("save/settings.wod")
-	#if settings_file.file_exists():
-		
+
+func load_player_data(save_file_name) -> bool:
+	var file_path = "save/" + save_file_name + ".json"
+	if FileAccess.file_exists(file_path):
+		var save_file = FileAccess.open(file_path, FileAccess.READ)
+		var save_text = save_file.get_as_text()
+		var save_data = JSON.parse_string(save_text)
+		if save_data is Dictionary:
+			player_data = save_data["player"]
+		else:
+			print("Save file corrupted, unable to read")
+			return false
+		return true
+	else:
+		print("Save file \"" + file_path + "\" could not be found")  
+		return false
