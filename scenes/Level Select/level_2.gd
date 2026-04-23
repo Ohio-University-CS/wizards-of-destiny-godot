4e279fb3f91cc90fc6285020d87d43ca6de2381d
CWestrick <142932514+CWestrick@users.noreply.github.com>
2026-04-23 11:33:36 -0400

    level select animations done

--- scenes/Level Select/level_2.gd
+++ scenes/Level Select/level_2.gd
@@ -3,8 +3,13 @@ extends Control
 @onready var forward_button : Button = $"Buttons/Forward Arrow"
 @onready var back_button : Button = $"Buttons/Back Arrow"
 @onready var level_select_button : Button = $"Buttons/Level 2"
+@onready var animatedsprite = $"Animation/AnimatedSprite2D"
+@onready var Buttons = $"Buttons"
 
 func _ready() -> void:
+	animatedsprite.speed_scale = 2.0  # 2x faster
+	await _animation_open()
+	
 	forward_button.pressed.connect(_on_forward_button_pressed)
 	back_button.pressed.connect(_on_back_button_pressed)
 	level_select_button.pressed.connect(_level_select_button_pressed)
@@ -25,10 +30,28 @@ func has_valid_back_button() -> bool:
 	return back_button != null
 
 func _on_forward_button_pressed():
+	await _animation_close()
 	get_tree().change_scene_to_file(get_forward_scene_path())
 
 func _on_back_button_pressed():
+	await _animation_close()
 	get_tree().change_scene_to_file(get_back_scene_path())
 
 func _level_select_button_pressed():
+	await _animation_close()
 	get_tree().change_scene_to_file("res://scenes/deckselect/deckselect_magician.tscn")
+
+func _animation_open():
+	Buttons.hide()
+	animatedsprite.show()
+	animatedsprite.play("Open")
+	await animatedsprite.animation_finished
+	animatedsprite.hide()
+	Buttons.show()
+
+func _animation_close():
+	Buttons.hide()
+	animatedsprite.show()
+	animatedsprite.play("Close")
+	await animatedsprite.animation_finished
+	animatedsprite.hide()
